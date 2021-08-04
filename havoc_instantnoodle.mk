858635e19fb6d8a2a8c798b7519153541f848c4f
Chris <cjh1249131356@gmail.com>
2021-08-04 21:16:49 +0800

    instantnoodle: Update to Redfin's August 2021 fingerprint

--- havoc_instantnoodle.mk
+++ havoc_instantnoodle.mk
@@ -46,4 +46,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus8 \
     PRIVATE_BUILD_DESC="OnePlus8-user 11 RP1A.201005.001 2012102310 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
